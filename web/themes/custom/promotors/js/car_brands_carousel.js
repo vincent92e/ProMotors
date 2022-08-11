2b12f3027814eaafcda056f3fd3755e5d33672a0
Vincent Ezeanaka <vincentezeanaka@Vincents-MacBook-Pro.local>
2022-08-11 13:54:17 -0400

    PM-20: added and enabled slick slider on the car brands paragraph type

---
+++ web/themes/custom/promotors/js/car_brands_carousel.js
@@ -0,0 +1,40 @@
+(function ($, window, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.carBrandsCarousel = {
+    attach: function (context, settings) {
+      $('.car-brands__content', context).slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 840,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+    }
+  };
+})(jQuery, window, Drupal);
