fdc28671d92fef9f761edf5e383baec3b547b5db
Vincent Ezeanaka <vincentezeanaka@Vincents-MacBook-Pro.local>
2022-09-12 13:49:03 -0400

    PM-26: created accordion js library, PM-26: deleted sidebar region from default page.html.twig template

--- web/themes/custom/promotors/js/accordion.js
+++ web/themes/custom/promotors/js/accordion.js
@@ -1,7 +1,16 @@
-if ($(".accordion__item__header").length > 0) {
-  var active = "active";
-  $(".accordion__item__header").click(function () {
-    $(this).toggleClass(active);
-    $(this).next("div").slideToggle(200);
-  });
-}
+(function ($, window, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.promotorsAccordion = {
+    attach: function (context, settings) {
+      if ($('.accordion-item__header', context).length > 0) {
+        let active = "active";
+
+        $('.accordion-item__header', context).click(function () {
+          $(this).toggleClass(active);
+          $(this).next("div").slideToggle(200);
+        });
+      }
+    }
+  };
+})(jQuery, window, Drupal);
