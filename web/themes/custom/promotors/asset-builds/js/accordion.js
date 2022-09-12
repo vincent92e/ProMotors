20f3c556a699b1c5435881a0f1e5efd1a0437b82
Vincent Ezeanaka <vincentezeanaka@Vincents-MacBook-Pro.local>
2022-09-12 13:46:50 -0400

    PM-26: added custom template for accordion paragraph type, PM-26: styled accordion paragraph type to match design requirments, PM-26: added custom js to open and close accordion component

---
+++ web/themes/custom/promotors/asset-builds/js/accordion.js
@@ -0,0 +1,2 @@
+"use strict";!function(t){Drupal.behaviors.promotorsAccordion={attach:function(i,o){0<t(".accordion-item__header",i).length&&t(".accordion-item__header",i).click(function(){t(this).toggleClass("active"),t(this).next("div").slideToggle(200)})}}}(jQuery,window);
+//# sourceMappingURL=accordion.js.map
